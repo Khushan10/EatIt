f8b93b6c27130133dcb3120fbf1fd5b7c9850662
khushan10 <khushan10112002@gmail.com>
2024-09-15 13:11:29 +0530

    Added custom password usage

---
+++ src/main/java/com/project/EatIt/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.project.EatIt.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="zpqopaebjqpoubgbyyaefqcbst";
+    public static final String JWT_HEADER="Authorization";
+}

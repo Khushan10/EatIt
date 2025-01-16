a28d93ba479700a5499ec0f2eb6cbcb8cf985f32
khushan10 <khushan10112002@gmail.com>
2025-01-16 19:49:18 +0530

    Added user register and login

--- src/main/java/com/project/EatIt/config/JwtConstant.java
+++ src/main/java/com/project/EatIt/config/JwtConstant.java
@@ -1,6 +1,6 @@
 package com.project.EatIt.config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY="zpqopaebjqpoubgbyyaefqcbst";
+    public static final String SECRET_KEY="your-very-secure-and-long-secret-key";
     public static final String JWT_HEADER="Authorization";
 }
---
+++ src/main/java/com/project/EatIt/response/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.project.EatIt.response;
+
+import com.project.EatIt.model.USER_ROLE;
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+    private String jwt;
+    private String message;
+    private USER_ROLE role;
+}

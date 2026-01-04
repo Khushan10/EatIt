b339fa32f012b0b7cc131b7bef1a19df879079ca
Khushan Chauhan <khushan10112002@gmail.com>
2026-01-04 23:45:27 +0530

    Ingredient service

---
+++ src/main/java/com/project/EatIt/repository/IngredientsCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.project.EatIt.repository;
+
+import com.project.EatIt.model.IngredientsCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IngredientsCategoryRepository extends JpaRepository<IngredientsCategory, Long> {
+    List<IngredientsCategory> findByRestaurantId(Long id);
+}
